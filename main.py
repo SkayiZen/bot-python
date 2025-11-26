223da35b1b8c86e07fe0e396c38cdcac72937db9
SkayiZen <103270112+SkayiZen@users.noreply.github.com>
2025-11-26 10:11:35 +0100

    🚧Added Activity

--- main.py
+++ main.py
@@ -14,19 +14,30 @@ logger = logging.getLogger('System')
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+activity = discord.Activity(
+    type=discord.ActivityType.listening,
+    name="Test activity"
+)
+
 class Client(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
         self.tree = app_commands.CommandTree(self)
-
+    
     async def on_ready(self):
         """Se déclenche une fois le bot connecté."""
+ 
+        await self.change_presence(
+            status=discord.Status.dnd,
+            activity=activity
+        )
+        
         logger.info(f'------------------------------------')
         logger.info(f'Bot connecté avec succès : {self.user}')
         logger.info(f'ID : {self.user.id}')
+        logger.info(f'Statut défini sur DND, Activité : {activity.name}')
         logger.info(f'Prêt à recevoir des commandes Slash.')
         logger.info(f'------------------------------------')
-        
 
 if __name__ == "__main__":
     if not TOKEN:
